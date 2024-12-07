61d17d2b7da21c3c707e3a5623525169fe40a1d8
k0ntinuum <145136324+k0ntinuum@users.noreply.github.com>
2024-12-06 20:30:12 -0500

    Add files via upload

---
+++ constant.c
@@ -0,0 +1,5 @@
+const int P = 256; // size of permutation vector
+const int B = 2; // base
+const int S = 8;
+const int place_value[8] = {128,64,32,16,8,4,2,1};
+//const int place_value[4] = {8,4,2,1};
